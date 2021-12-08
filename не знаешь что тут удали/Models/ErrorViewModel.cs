24d85e6c1335965a19f4ade555bfc916457a6dd6
allisongrand1 <yashenko.a12@gmail.com>
2021-12-08 23:56:33 +0500

    3d модель
    Пытаемся сохранить 3д файл в формате fbx через js скрипт

---
+++ не знаешь что тут удали/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace не_знаешь_что_тут_удали.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
